9151bfd1393fb46d627043ec5c44e3f59f71037d
Chrisi Hendricks <hendricks.christina@gmail.com>
2016-08-02 12:52:04 -0400

    Fix img interactivity so user can click off as well as on

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
     $('.menuItem').click(function () {
-        $(this).removeClass('minImg');
-        $(this).addClass('enlargeImg');
+        //$(this).removeClass('minImg');
+        //$(this).addClass('enlargeImg');
+        if ($(this).attr('class') === 'minImg') {
+            $(this).attr('class', 'enlargeImg');
+        }
+        else {
+            $(this).attr('class', 'minImg');
+        }
     });
 });
