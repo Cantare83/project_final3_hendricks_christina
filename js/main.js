e83483bc487a650329850d3a04c1a8c6a1d25c2e
Chrisi Hendricks <hendricks.christina@gmail.com>
2016-08-02 18:05:59 -0400

    Add JS to contact page to add directions content

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@ $(document).ready(function () {
     $('body').backstretch('img/woodbg.jpg');
 
 });
+
+var map = document.getElementById('location');
+
+function newParagraph() {
+    var newElement = document.createElement('p');
+    newElement.innerHTML = '<p>Directions to The Prancing Pony: <br> From the North, follow Snake Road down along the Bree River until you come to LadyBower Reservoir. We are located in the crook of the river. <br> From the South, follow A6013 north to Snake Road and turn left. Follow Snake Road and you cannot miss us</p>';
+    document.getElementsByTagName('aside')[0].appendChild(newElement);
+}
+
+map.addEventListener('click', newParagraph);
